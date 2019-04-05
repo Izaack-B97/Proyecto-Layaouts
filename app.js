7095a76545d0cfab3fa8865e41bc981e1f58f0d3
Isaac Bustamante <izaack.97@gmail.com>
2019-04-05 15:51:18 -0700

    Mongoose find(regresauna coleccion de documentos) y findOne(regresa un documento solamente del usuario que esta loggeandose)

--- app.js
+++ app.js
@@ -18,36 +18,56 @@ app.get('/', function(req,res){
     res.render('index');
 });
 
-app.get('/login', function(req,res){
+app.get('/signup', function(req,res){
     User.find(function(err,doc){
         console.log(doc);
         console.log("--------------------");
-        res.render('login');
+        res.render('signup');
     });
 });
 
+app.get('/login', function(req,res){
+        res.render('login');
+});
+
 app.post('/users', function(req,res){
-    let user=new User({
+    let modelUser = {
         "email": req.body.email, 
         "username": req.body.username,
         "password": req.body.password,
         "password_confirmation": req.body.password_confirmation
-    });
+    }
+   
+    let user=new User(modelUser);
 
-    console.log(user.password_confirmation);
+    console.log(user.password);
     
-
-    
-
-    user.save(function(err){
-        if (err)
-           console.log(String(err));
+    // user.save(function(err,user,numero){
+    //     if (err)
+    //        console.log(String(err));
  
-        res.send("Guardamos tus datos");
+    //     res.send("Guardamos tus datos");
+    // });
+    
+    /* PROMISES */
+    user.save()
+        .then(function(){
+            res.send("Guardamos tu informacion correctamente");
+        })
+        .catch(function(err){
+            res.send("No logramos guardar tu informacion, el error es: \n" + err);
+        });
+    
+    
+});
+
+app.post('/sessions', function(req,res){
+    /*query,fills(campos),callback */
+    User.findOne   ({ "email":req.body.email,"password":req.body.password },"",function(err,docs){
+         console.log(docs);
+         res.send("Bienvenido " + req.body.email + " !!!");    
+         
     });
-    
-    
-    
 });
 
 app.listen(8080,function(){ 
